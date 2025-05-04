7fea7217c8f0e1e55b57f281222c776363e095fe
huikkh <144972281+huikkh@users.noreply.github.com>
2025-05-04 10:51:27 +0800

    Add files via upload

---
+++ 打招呼程式.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    char name[100];
+
+    cin >> name;
+
+    cout << "Hello " << name << endl;
+    
+	return 0;
+}
