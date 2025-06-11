bb3b3d40cc65c6a798f4e28de8618d960f40359d
huikkh <144972281+huikkh@users.noreply.github.com>
2025-06-11 23:51:02 +0800

    Add files via upload

---
+++ 選擇角色.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int num;
+    cin >> num;
+
+    switch(num){
+        case 1 :
+            cout << "Person" << endl;
+            break;
+        case 2 :
+            cout << "Fairy" << endl;
+            break;
+        case 3 :
+            cout << "Dwarf" << endl;
+            break;    
+    }
+
+
+	return 0;
+}
