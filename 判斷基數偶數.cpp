b726f1e30db3479f7325fbbe907dbf6740c585c1
huikkh <144972281+huikkh@users.noreply.github.com>
2025-06-11 23:54:49 +0800

    Add files via upload

---
+++ 判斷基數偶數.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int num;
+    cin >> num;
+
+    if(num % 2 == 0){
+        cout << "even\n";
+    }
+    else{
+        cout << "odd\n";
+    }
+
+
+
+
+	return 0;
+}
